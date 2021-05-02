fe68697272eba921ac2dddfd087407381fc7009b
TejasNaik1 <83337086+TejasNaik1@users.noreply.github.com>
2021-05-02 16:09:25 +0530

    Swagger updated
    Swagger updated

--- src/main/java/com/capgemini/piapi/config/SwagerConfig.java
+++ src/main/java/com/capgemini/piapi/config/SwagerConfig.java
@@ -20,24 +20,10 @@ public class SwagerConfig {
 	@Bean
 	public Docket productApi() {
 		
-		//Configure Swagger and return Docket instace
+		//Configure Swagger and return Docket instance
 		return new Docket(DocumentationType.SWAGGER_2)		
 				.select().apis(RequestHandlerSelectors.basePackage("com.capgemini.piapi.web"))
 				.paths(PathSelectors.regex("/api.*"))				
-				.build()
-				.apiInfo(metoInfo());
-	}
-
-	private ApiInfo metoInfo() {
-		// Customize the Swagger output
-		ApiInfo apiInfo = new ApiInfo(
-				"Personal Project Management API", 
-				"PPM API Created by Codegram", 
-				"1.0", 
-				"Terms of Service", 
-				new Contact("Pankaj Sharma", "https://www.codegram.in/", "pankajsimmc@gmail.com"), 
-				"CODEGRAM Licence v.1.0", 
-				"https://www.codegram.in/", new ArrayList<>());
-		return apiInfo;
+				.build();
 	}
 }
