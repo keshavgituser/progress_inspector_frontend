a3c7ac71a36dca4f868aa67a530017461eaac2bb
Aadesh Juvekar <juvekaraadesh66@gmail.com>
2021-05-04 08:24:31 +0530

    Product Owner Updated

--- src/main/java/com/capgemini/piapi/domain/ProductOwner.java
+++ src/main/java/com/capgemini/piapi/domain/ProductOwner.java
@@ -12,7 +12,6 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 /**
@@ -44,7 +43,6 @@ public class ProductOwner {
 	 * password of the ProductOwner
 	 */
 	@NotNull(message = "Password is required")
-	@Size(min=8, max=20,message = "Please enter valid password")
 	private String pwd;
 	/**
 	 * List of the tasks in the sprint
